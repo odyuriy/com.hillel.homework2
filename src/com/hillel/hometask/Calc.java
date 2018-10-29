a44903b61247afd93f579de2fef30c9868f68cb0
unknown <H:\TheBat!>
2018-10-29 17:38:19 +0200

    Initial commit

---
+++ src/com/hillel/hometask/Calc.java
@@ -0,0 +1,20 @@
+package com.hillel.hometask;
+
+public class Calc {
+    public static int add(int x, int y) {
+        int result = x + y;
+        return result;
+    }
+    public static int sub(int x, int y) {
+        int resul = x - y;
+        return resul;
+    }
+    public static int div(int x, int y) {
+        int resu = x / y;
+        return resu;
+    }
+    public static int mul(int x, int y) {
+        int res = x * y;
+        return res;
+    }
+}
---
+++ src/com/hillel/hometask/Main.java
@@ -0,0 +1,15 @@
+package com.hillel.hometask;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int result = Calc.add(5,7);
+        System.out.println(result);
+        int resul = Calc.sub(100,35);
+        System.out.println(resul);
+        int resu = Calc.div(200,50);
+        System.out.println(resu);
+        int res = Calc.mul(33,3);
+        System.out.println(res);
+    }
+}
